71907a369e5c58966050b275963cf036b9306133
hamedsalim <hamediran1999@gmail.com>
2021-04-05 15:39:33 +0430

    fix body of comment models

--- backend/core/card/submodels/comment_models.py
+++ backend/core/card/submodels/comment_models.py
@@ -4,9 +4,9 @@ from .base_modles import Timestamp
 from .card_models import CardInfo
 
 class CardComment(Timestamp):
-    for_card = models.ForeignKey(CardInfo)
+    for_card = models.ForeignKey(CardInfo,on_delete=models.CASCADE)
     name = models.CharField(max_length=515,null=True,blank=True)
-    body = models,TextField()
+    body = models.TextField()
 
     def __str__(self):
-        return self.for_card
+        return self.name
