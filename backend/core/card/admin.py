b6610da2a8fc9e7c0b8121d3e991b085ec84dedd
hamedsalim <hamediran1999@gmail.com>
2021-04-05 10:12:08 +0430

    addd admin panel

--- backend/core/card/admin.py
+++ backend/core/card/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import CardInfo,CardComment
 # Register your models here.
+
+@admin.register(CardInfo)
+class CardInfoAdmin(admin.ModelAdmin):
+    '''Admin View for CardInfo'''
+    pass
+
+@admin.register(CardComment)
+class CardCommentAdmin(admin.ModelAdmin):
+    '''Admin View for CardComment'''
+
+    pass
