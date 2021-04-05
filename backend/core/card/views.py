5f2a79d165b38039fa57374d7f94568ae6474a5c
hamedsalim <hamediran1999@gmail.com>
2021-04-05 16:21:37 +0430

    write card and card comment api view

--- backend/core/card/views.py
+++ backend/core/card/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+import re
+from django.db.models import query
+from .models import CardComment,CardInfo
+from .serializers import CardInfoSerializer,CardCommentSerializer
+from rest_framework.generics import ListAPIView,ListCreateAPIView
 
-# Create your views here.
+
+
+class CardInfoListView(ListAPIView):
+    queryset = CardInfo.objects.all()
+    serializer_class = CardInfoSerializer
+    
+
+class CardCommentListCreateAPI(ListCreateAPIView):
+    queryset = CardComment.objects.all()
+    serializer_class = CardCommentSerializer
