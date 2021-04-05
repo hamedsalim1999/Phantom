21e54b8d95f5b7c781cb7069ada5be1d1bb8d5d0
hamedsalim <hamediran1999@gmail.com>
2021-04-05 10:09:10 +0430

    write card models

---
+++ backend/core/card/submodels/card_models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.db.models.fields import TextField
+from .base_modles import Timestamp
+
+class CardInfo(Timestamp):
+    name = models.CharField(max_length=256)
+    description = models.TextField()
+    image = models.ImageField(max_length=128,upload_to='blog/post/image', height_field='height_field', width_field='width_field',blank=True, null=True)
+    height_field = models.PositiveIntegerField(default='480',blank=True, null=True)
+    width_field  = models.PositiveIntegerField(default='720',blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+
+        verbose_name = 'CardInfo'
+        verbose_name_plural = 'CardInfoes'
