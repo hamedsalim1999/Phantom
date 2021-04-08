905a5e1a89f23160e9590ef58995e232c7fc03b1
hamedsalim <hamediran1999@gmail.com>
2021-04-08 12:57:39 +0430

    add bootstrap and card and read data from api

--- frontend/core/src/App.vue
+++ frontend/core/src/App.vue
@@ -1,18 +1,26 @@
 <template>
 
+ <div >
+ <Navbar/>>
+<Card/>
+</div>
+
 </template>
 
 <script>
 
+import Card from "./components/Card"
 
 export default {
  
   components: {
-  
+  Card,
   }
 }
 </script>
 
 <style>
+@import url('./assets/css/bootstrap.min.css');
+@import url('./assets/css/bootstrap.css');
 
 </style>
--- frontend/core/src/components/Card.vue
+++ frontend/core/src/components/Card.vue
@@ -1,18 +1,72 @@
 <template>
+<main>
+  <div class="album py-5 bg-light" >
+    <div class="container">
+      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+        <div class="col" v-for="info in infos" v-bind:key ="info.id">
+          <div class="card shadow-sm">
+            <img :src="info.image" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">{{info.name}}</h5>
+                <p class="card-text">{{info.description}}</p>
+              </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</main>
+
+
+
+
+
+
+
 
 </template>
 
 <script>
 
-
+import axios from 'axios'
 export default {
- 
-
-  
+  data () {
+    return {
+      infos:[]
+    }
+  },
+  mounted () {
+this.getCard()
+  },
+methods:{
+  getCard(){
+  axios({
+    method:'get',
+    url:'http://127.0.0.1:8000/',
+    auth:{
+      username:'hamed',
+      password:'hamed123'
+    }
+  }).then(res => this.infos = res.data)
+}
+  }
   }
 
 </script>
-
 <style>
+
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
 
 </style>
